96c420652485fa7284232516d3cf65f8977a457b
Moyinoluwa Michael <ogunsmoyin.m@gmail.com>
2023-03-20 07:31:20 +0100

    Initial commit

--- src/main/java/michael/moyinoluwa/emailApp/dtos/request/RegisterRequest.java
+++ src/main/java/michael/moyinoluwa/emailApp/dtos/request/RegisterRequest.java
@@ -1,5 +1,8 @@
 package michael.moyinoluwa.emailApp.dtos.request;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
     private String firstName;
     private String lastName;
@@ -8,60 +11,4 @@ public class RegisterRequest {
     private String dayOfBirth;
     private String monthOfBirth;
     private String yearOfBirth;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getDayOfBirth() {
-        return dayOfBirth;
-    }
-
-    public void setDayOfBirth(String dayOfBirth) {
-        this.dayOfBirth = dayOfBirth;
-    }
-
-    public String getMonthOfBirth() {
-        return monthOfBirth;
-    }
-
-    public void setMonthOfBirth(String monthOfBirth) {
-        this.monthOfBirth = monthOfBirth;
-    }
-
-    public String getYearOfBirth() {
-        return yearOfBirth;
-    }
-
-    public void setYearOfBirth(String yearOfBirth) {
-        this.yearOfBirth = yearOfBirth;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
